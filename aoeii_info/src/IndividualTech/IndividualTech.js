8b88280758e8364caedf460ca54210f99f99c00d
Jim Chen <jimchen@Jims-Air.fios-router.home>
2020-06-30 13:27:11 -0400

    techs added

--- aoeii_info/src/IndividualTech/IndividualTech.js
+++ aoeii_info/src/IndividualTech/IndividualTech.js
@@ -6,6 +6,7 @@ function IndividualUnits (props){
     const [techCost, setTechCost]= useState({})
     const [techDesc, setTechDesc]=useState('')
     const [techLoc, setTechLoc]=useState('')
+    const [techDetail, setTechDetail]=useState({})
     // props.handleUnitName(props.match.params.unit)
     useEffect( ()=>{
         let IndtechURL=`https://cors-anywhere.herokuapp.com/https://age-of-empires-2-api.herokuapp.com/api/v1/technology/${props.match.params.tech}`
@@ -14,46 +15,46 @@ function IndividualUnits (props){
             const json = await res.json()
             console.log('ind tech', json)
             console.log(json.age)
+            setTechDetail(json)
             setTechAge(json.age)
             setTechCost(json.cost)
             setTechDesc(json.description)
-            setTechLoc(json.develops_in.slice(55))
-            console.log('techage',techAge)
-            console.log('tech cost',techCost)
-            console.log('tech desc',techDesc)
-            console.log('tech Loc',techLoc)
+            setTechLoc(json.develops_in.slice(60))
         }
         makeAPICall()
     },[props.match.params.tech] )
 
-    // let displayUnitAttackBonus = <h1>loading</h1>;
-    // if (unitAttackBonus[0]) {
-    //   displayUnitAttackBonus = unitAttackBonus.map((bonus) => {
-    //     return <p>{bonus}</p>;
-    //   });
-    // }
-  //   let displayUnitCost=<p>Loading Cost</p>
-  //   if(unitCost){
-  //   if(!unitCost.Food){
-  //     displayUnitCost=<p>Wood: {unitCost.Wood} Gold: {unitCost.Gold}</p>
-  //   } else if (!unitCost.Wood){
-  //     displayUnitCost=<p>Food: {unitCost.Food} Gold: {unitCost.Gold}</p>
-  //   } else if (!unitCost.Gold){
-  //     displayUnitCost=<p>Food: {unitCost.Food} Wood: {unitCost.Wood}</p>
-  //   } else {
-  //     displayUnitCost=<p>Food: {unitCost.Food} Wood: {unitCost.Wood} Gold: {unitCost.Gold}</p>
-  //   }
-  // }
-    
+    let displayTechCost = <h1>loading</h1>;
+    let woodCost=''
+    let foodCost=''
+    let goldCost=''
+    let stoneCost=''
+
+    if (techDetail) {
+      if(techCost.Wood){
+        woodCost=`Wood: ${techCost.Wood}`
+      }
+      if (techCost.Food) {
+        foodCost=`Food: ${techCost.Food}`
+      }
+      if (techCost.Gold) {
+        goldCost=`Gold: ${techCost.Gold}`
+      }
+      if (techCost.Stone) {
+        stoneCost=`Stone: ${techCost.Stone}`
+      }
+    }
+    if(techDetail){
+      displayTechCost=<h2>Cost- {foodCost} {woodCost} {goldCost} {stoneCost}</h2>
+    }
     return (
-        <h1>im ind tech</h1>
-        // <div className='ind-unit'>
-        // <h1>{unitDetail.name}</h1>
-        // <h2>'{unitDetail.description}'</h2>
-        // <h2>Attack: {unitDetail.attack}</h2>
-        // <h2>Armor(Melee/Pierce): {unitDetail.armor}</h2>
-        // {displayUnitCost}
-        // </div>
+        <div className='ind-unit'>
+          <h1>{props.match.params.tech}</h1>
+          <h2>'{techDesc}'</h2>
+          {displayTechCost}
+          <h2>Available at the {techLoc}</h2>
+          <h2>Available during {techAge} Age</h2>
+        </div>
     )
 }
 export default IndividualUnits
